cdb5d9956e91bd450a30bceab53495ec6a431361
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-03-14 16:07:52 +0900

    JsonValue

--- yaxel-server/Meta.fs
+++ yaxel-server/Meta.fs
@@ -61,3 +61,28 @@ let rec toMetaType (t: System.Type) =
         |> Union
     else
         Unknown t
+
+type JsonValue =
+    | JsonNull
+    | JsonNumber of float
+    | JsonString of string
+    | JsonArray of JsonValue []
+    | JsonObject of Map<string, JsonValue>
+    override this.ToString() =
+        match this with
+        | JsonNull -> "null"
+        | JsonNumber x -> x.ToString()
+        | JsonString s -> sprintf "\"%s\"" s
+        | JsonArray a ->
+            a
+            |> Array.map (fun x -> x.ToString())
+            |> String.concat ","
+            |> sprintf "[%s]"
+        | JsonObject x ->
+            x
+            |> Map.toSeq
+            |> Seq.map (fun (key, value) -> sprintf "\"%s\":%O" key value)
+            |> String.concat ","
+
+
+let toJsonValue (t: Type) = JsonNull

001529bc4f93dae6e6f2cdc00bbd36ca24cf3507
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-03-14 21:25:56 +0900

    fetch function/hoge from React Component

--- yaxel-client/src/App.tsx
+++ yaxel-client/src/App.tsx
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+interface State {
+  text: string
+}
+
+class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { text: "hoge" };
+  }
+  componentDidMount() {
+    fetch('function/hoge')
+      .then(response => response?.text())
+      .then(t => this.setState({ text: t }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <p>aaa bbb</p>
+          <p>{this.state.text}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+
+  }
 }
 
 export default App;

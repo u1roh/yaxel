926c0c6fa49eb8d4b871802822bf628358c29b14
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-03-18 21:50:41 +0900

    FunArgsInput component

--- yaxel-client/src/Function.tsx
+++ yaxel-client/src/Function.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import TypedInput from './TypedInput'
 import * as yaxel from './yaxel'
 
+class FunArgsInput extends React.Component<{ params: yaxel.TypedItem[] | undefined }> {
+    render() {
+        return (<form>
+            {this.props.params?.map(p => <TypedInput name={p.name} type={p.type} />)}
+            <input type="submit"></input>
+        </form>);
+    }
+}
+
 interface FunctionProps {
     name: string
 }
@@ -25,13 +34,10 @@ class Function extends React.Component<FunctionProps, FunctionState> {
             <div className="Function">
                 <hr></hr>
                 <h2>'<span className="Function-name">{this.state.func?.name}</span>' function</h2>
+                <h3>params</h3>
+                <FunArgsInput params={this.state.func?.params}></FunArgsInput>
                 <h3>return</h3>
                 <p>{JSON.stringify(this.state.func?.ret)}</p>
-                <h3>params</h3>
-                {this.state.func?.params.map(p =>
-                    <div>
-                        <TypedInput name={p.name} type={p.type} />
-                    </div>)}
             </div>
         );
     }

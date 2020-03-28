8f15078d9d2c1e50598150bc05178879621f3940
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-03-28 14:22:12 +0900

    refactoring

--- yaxel-client/src/api.ts
+++ yaxel-client/src/api.ts
@@ -1,5 +1,15 @@
 import * as yaxel from './yaxel'
 
+function postJson(input: RequestInfo, json: string): Promise<Response> {
+    return fetch(input, {
+        method: 'POST',
+        body: json,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
 async function fetchText(input: RequestInfo): Promise<string> {
     const res = await fetch(input);
     return res.text();
@@ -23,25 +33,13 @@ export function fetchUserCode(): Promise<string> {
     return fetchText('api/usercode/');
 }
 
-export function updateUserCode(code: string) {
-    fetch('api/update-usercode', {
-        method: 'POST',
-        body: code,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+export function updateUserCode(code: string): Promise<Response> {
+    return postJson('api/update-usercode', code);
 }
 
 export async function invokeFunction(funcName: string, args: any[]): Promise<any> {
     console.log("api.invokeFunction(" + funcName + ", " + JSON.stringify(args) + ")");
-    const response = await fetch('api/invoke/' + funcName, {
-        method: 'POST',
-        body: JSON.stringify(args),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    const response = await postJson('api/invoke/' + funcName, JSON.stringify(args));
     const txt = await response.text();
     try {
         return JSON.parse(txt);
